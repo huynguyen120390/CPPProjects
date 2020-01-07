93adba6d23a64a7b55ab28afe5e434c9a5b6ade8
Huy Nguyen <hnguyen@kymetacorp.com>
2020-01-06 19:36:27 -0800

    No Copy Policy

---
+++ PracticeBasics/MemoryManagement/policy_nocopy.cpp
@@ -0,0 +1,31 @@
+class NoCopyClass1{
+    private: 
+        // Hack: Try to declare private constructor and operator for the class itself
+        // then we cannot copy these classes by accessing to these classes 
+        NoCopyClass1(const NoCopyClass1 &);
+        NoCopyClass1 &operator=(const NoCopyClass1 &);
+    public:
+        NoCopyClass1(){};
+};
+
+class NoCopyClass2{
+    public:
+        NoCopyClass2(){}
+        // Hack: Try to link the class copy syntaxes to delete function
+        // This hack is more explicit, clearer to programmer
+        NoCopyClass2(const NoCopyClass2 &) = delete;
+        NoCopyClass2 &operator=(const NoCopyClass2 &) = delete;
+};
+
+int main(){
+    //The following code are caught by editor , so just comment out , but usable to understand NoCopy Policy
+    //NoCopyClass1 original1;
+    //NoCopyClass1 copy1a(original1); //error:private constructor
+    //NoCopyClass1 copy1b= original1; //error:private operator
+
+    //NoCopyClass2 original2;
+    //NoCopyClass2 copy2a(original2); //error:use of deleted function ‘NoCopyClass2::NoCopyClass2(const NoCopyClass2&)’
+    //NoCopyClass2 copy2b= original2; //error:use of deleted function ‘NoCopyClass2::NoCopyClass2(const NoCopyClass2&)’
+    return 0; 
+}
+
