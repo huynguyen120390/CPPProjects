ea10b1b7390066b5d1d7a2cd83bcd8a43f4bd0ab
huy <huynguyen120390@gmail.com>
2020-03-18 12:03:42 -0700

    Add script of pass_data_from_main_to_threads.cpp

---
+++ PracticeBasics/Concurrency/2_PassingDataBetweenThreads/pass_data_from_main_to_threads.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+
+void print_message(std::string message){
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::cout << "Thread 1: " << message << std::endl;
+}
+
+int main(){
+    //define message 
+    std::string message = "My Message";
+
+    //start thread using variadic templates
+    std::thread t1(print_message,message);
+
+    //start thread using a Lambda
+    std::thread t2([message]{
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::cout << "Thread 2" << message << std::endl;
+    });
+
+    //thread barrier
+    t1.join();
+    t2.join();
+}
